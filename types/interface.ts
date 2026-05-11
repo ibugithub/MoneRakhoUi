41088c61e0ea7e3002cb0edda4a30ed0b91d3f69
ibugithub <outsideworkibrahim@gmail.com>
2026-05-11 12:25:28 +0600

    added next monerakho frontend

---
+++ types/interface.ts
@@ -0,0 +1,11 @@
+interface User {
+  email: string;
+  signUp_by: "email" | "google" | "facebook" | "microsoft";
+}
+export interface ProfileInterface {
+  user: User;
+  Display_name: string | null;
+  profile_picture: string | null;
+  created_at: string; 
+  updated_at: string;
+}
